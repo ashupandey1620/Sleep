ac63a8e8921436ced441c53c5a259cc2dbf3a1a8
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-05-04 20:24:17 +0530

    Sleep Disturbance Screen

---
+++ app/src/main/java/com/ashutosh/sleep/Components/SupportActionItemSD.kt
@@ -0,0 +1,87 @@
+package com.ashutosh.sleep.Components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.ashutosh.sleep.R
+
+@ExperimentalMaterial3Api
+@Composable
+fun SupportAccountItemSD(
+    icon: Int , mainText: String , onClick: () -> Unit ,
+    navController: NavController
+) {
+
+    Column(
+        modifier = Modifier
+            .padding(bottom = 18.dp)
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clip(RoundedCornerShape(10.dp))
+
+    ) {
+
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+                .background(Color.Black.copy(alpha = 0.6f))
+                ,verticalAlignment = Alignment.CenterVertically) {
+                
+                Spacer(modifier = Modifier.padding(10.dp))
+                Box(
+                    modifier = Modifier
+                        .size(25.dp) ,
+                    contentAlignment = Alignment.Center
+                ) {
+                    Icon(
+                        painter = painterResource(id = icon) ,
+                        contentDescription = "",
+                        tint = Color.White,
+                    )
+                }
+
+                Spacer(modifier = Modifier.width(14.dp))
+
+                Column(
+                    modifier = Modifier
+                ) {
+                    Text(
+                        text = mainText,
+                        color = Color.White,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Medium,
+                    )
+                }
+            }
+
+
+
+    }
+}
--- app/src/main/java/com/ashutosh/sleep/Screens/SleepDisturbanceScreen.kt
+++ app/src/main/java/com/ashutosh/sleep/Screens/SleepDisturbanceScreen.kt
@@ -3,19 +3,34 @@ package com.ashutosh.sleep.Screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.ashutosh.sleep.Components.SupportAccountItem
+import com.ashutosh.sleep.Components.SupportAccountItemSD
 import com.ashutosh.sleep.Components.Toolbar
+import com.ashutosh.sleep.Navigation.Routes
 import com.ashutosh.sleep.R
 import com.ashutosh.sleep.ui.theme.SleepTheme
 
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun SleepDisturbanceScreen(navController: NavController) {
@@ -42,6 +57,79 @@ fun SleepDisturbanceScreen(navController: NavController) {
                         .fillMaxSize() ,
                     contentScale = ContentScale.FillBounds
                 )
+
+                Column(modifier = Modifier
+                    .fillMaxSize()
+                    .padding(horizontal = 20.dp)) {
+
+                    Spacer(modifier = Modifier.fillMaxWidth()
+                        .height(60.dp))
+
+                    Text(
+                        text = "Tap to add/remove the reason for speed disturbances",
+                        color = Color.White,
+                        fontSize = 22.sp,
+                        fontWeight = FontWeight.Bold,
+                    )
+
+                    Spacer(modifier = Modifier.fillMaxWidth()
+                        .height(10.dp))
+
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.addother,
+                        mainText = "Add Other",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.dream,
+                        mainText = "Dream",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.kids,
+                        mainText = "Kids",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.love,
+                        mainText = "Love",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.water,
+                        mainText = "Water",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+
+                    SupportAccountItemSD(
+                        icon = R.drawable.tiolet,
+                        mainText = "Toilet",
+                        onClick = {
+
+                        },
+                        navController
+                    )
+                }
 
             }
 
