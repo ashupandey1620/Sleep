edf2a3db19cd65db2b38df4d5857ab393c548be2
Ashutosh Pandey <ashupandey1620@gmail.com>
2024-05-05 02:54:00 +0530

    Goal Screen

---
+++ app/src/main/java/com/ashutosh/sleep/Components/SupportActionGoal.kt
@@ -0,0 +1,112 @@
+package com.ashutosh.sleep.Components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.ashutosh.sleep.R
+
+
+@ExperimentalMaterial3Api
+@Composable
+fun SupportActionGoal(
+    icon: Int , mainText: String , supportText: String, onClick: () -> Unit ,
+    navController: NavController
+) {
+
+    val color = remember{ mutableStateOf(false) }
+
+    Box(modifier = Modifier
+        .fillMaxWidth()
+        .height(160.dp)
+        .padding(4.dp)) {
+
+        Image(
+            painter = painterResource(id = icon) ,
+            contentDescription = "" ,
+            modifier = Modifier
+                .fillMaxSize() ,
+            contentScale = ContentScale.FillBounds
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(6.dp)
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .clip(RoundedCornerShape(10.dp))
+
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(20.dp) , verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.End
+            ) {
+
+                Icon(
+                    painter = painterResource(id = R.drawable.like) ,
+                    contentDescription = "" ,
+                    tint = if (!color.value)Color.White else Color.Red ,
+                    modifier = Modifier.clickable {
+                        color.value = !color.value
+                    }
+                )
+            }
+
+            Spacer(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(80.dp)
+            )
+
+
+            Text(
+                text = mainText ,
+                color = Color.White ,
+                fontSize = 15.sp ,
+                lineHeight = 17.sp ,
+                fontWeight = FontWeight.Medium ,
+            )
+
+            Text(
+                text = supportText ,
+                color = Color.White ,
+                fontSize = 12.sp ,
+                lineHeight = 14.sp ,
+                fontWeight = FontWeight.Medium ,
+            )
+
+
+        }
+    }
+}
--- app/src/main/java/com/ashutosh/sleep/Screens/GoalScreen.kt
+++ app/src/main/java/com/ashutosh/sleep/Screens/GoalScreen.kt
@@ -3,12 +3,19 @@ package com.ashutosh.sleep.Screens
 import android.annotation.SuppressLint
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -22,14 +29,49 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.ashutosh.sleep.Components.SupportActionGoal
 import com.ashutosh.sleep.Components.Toolbar
 import com.ashutosh.sleep.R
 import com.ashutosh.sleep.ui.theme.SleepTheme
+import com.google.gson.Gson
+import java.net.URLEncoder
 
+data class SupportAction(
+    val icon: Int,
+    val mainText: String,
+    val supportText: String
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun GoalScreen(navController:NavController) {
     val painter: Painter = painterResource(id = R.drawable.back)
+
+
+    val ItemsList = listOf(
+
+        SupportAction(
+            R.drawable.girl,
+            "De-stress",
+            "NightTime"),
+
+        SupportAction(
+            R.drawable.boy,
+        "Fall Asleep",
+        "NightTime"),
+
+        SupportAction(
+            R.drawable.girl,
+            "Take a break",
+            "NightTime"),
+
+        SupportAction(
+            R.drawable.boy,
+            "clear your mind",
+            "NightTime")
+
+    )
 
     Scaffold(modifier = Modifier.fillMaxSize() ,
         topBar = {
@@ -39,7 +81,8 @@ fun GoalScreen(navController:NavController) {
             )
         } ,
         bottomBar = {
-            Button(shape = RoundedCornerShape(5.dp) , modifier = Modifier.padding(10.dp)
+            Button(shape = RoundedCornerShape(5.dp) , modifier = Modifier
+                .padding(10.dp)
                 .fillMaxWidth()
                 .padding(5.dp), onClick = { /*TODO*/ },
                 colors = ButtonDefaults.buttonColors(Color(0xFF0277bd))) {
@@ -67,6 +110,49 @@ fun GoalScreen(navController:NavController) {
                 )
 
             }
+
+
+           Column(modifier = Modifier.padding(10.dp)) {
+
+               Spacer(modifier = Modifier
+                   .fillMaxWidth()
+                   .height(60.dp))
+
+               Text(
+                   text = "Select the Goal you want to achieve",
+                   color = Color.White,
+                   fontSize = 19.sp,
+                   lineHeight = 20.sp,
+                   fontWeight = FontWeight.Medium,
+               )
+
+               LazyVerticalGrid(
+                   modifier = Modifier
+                       .fillMaxSize(),
+                   columns = GridCells.Fixed(2) ,
+                   userScrollEnabled = true ,
+
+                   contentPadding = PaddingValues(
+                       start = 12.dp ,
+                       top = 16.dp ,
+                       end = 12.dp ,
+                       bottom = 16.dp
+                   ) ,
+                   content = {
+
+                       items(ItemsList.size) { index ->
+                           SupportActionGoal(
+                               ItemsList[index].icon ,
+                               ItemsList[index].mainText ,
+                               ItemsList[index].supportText ,
+                               { } ,
+                               navController
+                           )
+                       }
+
+                   }
+               )
+           }
 
         }
     )
